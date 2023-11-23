2807e515a372974e99b3eb5591e79935f0b2799a
chichoon <chichoon.choi@gmail.com>
2023-11-23 16:53:03 +0900

    feat: :sparkles: Gauge chart init

---
+++ src/components/GaugeChart/GaugeChart.stories.ts
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/vue3';
+
+import GaugeChart from './GaugeChart.vue';
+
+const meta = {
+  title: 'Gauge Chart',
+  component: GaugeChart,
+  tags: ['chart'],
+  argTypes: {
+    color: { control: 'color' }
+  },
+  args: { color: '#FF0000' }
+} satisfies Meta<typeof GaugeChart>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    color: '#FF0000'
+  }
+};
---
+++ src/components/GaugeChart/GaugeChart.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+interface Props {
+  color: string;
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+  <div>aaa</div>
+</template>
+
+<style scoped lang="scss"></style>
