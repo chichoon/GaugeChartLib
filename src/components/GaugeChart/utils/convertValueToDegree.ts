511eb272604f44c4cb3397b758c80b69613f97bd
chichoon <chichoon.choi@gmail.com>
2023-11-24 14:06:00 +0900

    feat: :sparkles: 값과 최대값을 받아 게이지 차트의 각도를 조절하는 함수

---
+++ src/components/GaugeChart/utils/convertValueToDegree.ts
@@ -0,0 +1,11 @@
+export function convertValueToDegree(value: number, maxValue: number) {
+  const ratio = value / maxValue;
+  const degree = ratio * 270;
+  console.log(degree);
+
+  if (degree <= 45) return degree + 135;
+  if (degree <= 225) return -(225 - degree);
+  return degree - 225;
+}
+
+// value 50 maxValue 100 => 0.5
