ffc6659c6fcbf9625f8182567ce0a56d5efc911f
chichoon <chichoon.choi@gmail.com>
2023-11-26 00:03:43 +0900

    feat: :sparkles: drawText 에 외곽선 기능 추가

--- src/components/GaugeChart/utils/drawText.ts
+++ src/components/GaugeChart/utils/drawText.ts
@@ -3,10 +3,19 @@ export function drawText(
   x: number,
   y: number,
   fontSize: number,
-  text: string
+  text: string,
+  textColor?: string,
+  strokeWidth?: number,
+  strokeColor?: string
 ) {
   ctx.font = `${fontSize}px Arial`;
   ctx.textAlign = 'center';
-  ctx.fillStyle = 'rgba(0, 0, 0, 1)';
+  ctx.textBaseline = 'middle';
+  if (strokeWidth && strokeColor) {
+    ctx.lineWidth = strokeWidth;
+    ctx.strokeStyle = strokeColor;
+    ctx.strokeText(text, x, y);
+  }
+  ctx.fillStyle = textColor ?? 'black';
   ctx.fillText(text, x, y);
 }
