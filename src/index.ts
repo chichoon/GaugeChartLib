4be2ce0ef2a3598a31ff1a3cc66eb158ab756f2c
chichoon <chichoon.choi@gmail.com>
2023-11-27 10:24:18 +0900

    build: :building_construction: 내보내기 수정

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,3 @@
-import type { App } from 'vue';
 import GaugeChart from './components/GaugeChart';
 
-const components = [GaugeChart];
-
-const install = (app: App) => {
-  if (!app) return;
-  components.forEach((component) => {
-    app.component(component.name, component);
-  });
-};
-
-const GaugeChartLib = {
-  install
-};
-
 export { GaugeChart };
-
-export default GaugeChartLib;
